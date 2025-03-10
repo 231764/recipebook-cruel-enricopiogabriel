bb1878946e5c370e6ee94af67f0565d24512461d
Enrico Cruel <pio.cruel@student.ateneo.edu>
2025-03-10 13:34:20 +0800

    Added Profile model and modified Recipe model with author, createdon, updatedon fields

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #one to one is a foreign key with the restriction that there is one profile instance that can point to a user instance
+    short_bio = models.TextField(max_length=255)
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
@@ -12,6 +17,12 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
+    author = models.ForeignKey(Profile, 
+        on_delete = models.CASCADE,
+        null=True,
+        blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)  # Sets date and time on creation
+    updated_on = models.DateTimeField(auto_now=True)  # Updates date and time on save
 
     def __str__(self): 
         return self.name
