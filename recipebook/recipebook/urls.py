b036e2dce17de9a95b0e867d487f5429244bb189
Enrico Cruel <pio.cruel@student.ateneo.edu>
2025-03-10 14:51:12 +0800

    Modified urls.py to include user-login-related htmls and settings.py to redirect login and logout links

--- recipebook/recipebook/urls.py
+++ recipebook/recipebook/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('ledger.urls', namespace="ledger")),  # Include ledger URLs
     path('admin/', admin.site.urls),
+     path('accounts/', include('django.contrib.auth.urls'))
 ]
