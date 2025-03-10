52c870cd53dd56403f12475aa5f0f1ae329edcfb
Enrico Cruel <pio.cruel@student.ateneo.edu>
2025-03-10 22:12:59 +0800

    Modified files for debugging

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,15 +1,5 @@
 from django.contrib import admin
 from .models import Recipe, RecipeIngredient, Ingredient
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.models import User
-from .models import Profile
-
-class ProfileInline(admin.StackedInline):
-    model = Profile
-    can_delete = False
-
-class UserAdmin(BaseUserAdmin): 
-    inlines = [ProfileInline,]
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
@@ -17,9 +7,9 @@ class RecipeIngredientInline(admin.TabularInline):
 
 class RecipeAdmin(admin.ModelAdmin):
     model = Recipe
-    search_fields = ('name', 'author__username')  # Allow searching by author
-    list_display = ('name', 'author', 'created_on', 'updated_on')  
-    list_filter = ('name', 'author', 'created_on', 'updated_on')  
+    search_fields = ('name',)  # Allow searching by author
+    list_display = ('name',)  
+    list_filter = ('name', 'ingredients',)  
     inlines = [RecipeIngredientInline]  # Allows editing RecipeIngredients in Recipe Admin
 
     fieldsets = [
@@ -32,13 +22,6 @@ class IngredientAdmin(admin.ModelAdmin):
     list_display = ('name',) 
     search_fields = ('name',)
 
-class ProfileAdmin(admin.ModelAdmin):
-    model = Profile
-    list_display = ('user', 'short_bio')
-    search_fields = ('user__username', 'short_bio')
 
-admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
-admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -2,13 +2,6 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) #one to one is a foreign key with the restriction that there is one profile instance that can point to a user instance
-    short_bio = models.TextField(max_length=255)
-
-    def __str__(self): 
-        return self.user.username
-
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
 
@@ -20,7 +13,8 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
-    author = models.ForeignKey(Profile, 
+    author = models.ForeignKey(
+        User, 
         on_delete = models.CASCADE,
         null=True,
         blank=True)
@@ -35,12 +29,14 @@ class Recipe(models.Model):
 
 class RecipeIngredient(models.Model):
     quantity = models.CharField(max_length=255)
-    ingredient = models.ForeignKey(Ingredient, 
+    ingredient = models.ForeignKey(
+        Ingredient, 
         on_delete=models.SET_NULL, 
         null=True,
         related_name='recipe'
     ) 
-    recipe = models.ForeignKey(Recipe, 
+    recipe = models.ForeignKey(
+        Recipe, 
         on_delete=models.SET_NULL, 
         null=True,
         related_name='ingredients'
