ca806699994e71fdfe1ba1396c2173cc3868e5fe
Enrico Cruel <pio.cruel@student.ateneo.edu>
2025-03-24 17:58:26 +0800

    Adjusted ledger template .html and urls.py and views.py to support adding recipes and adding images, added add_forms.html and forms.py for forms to create adding recipes and adding images

--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,11 +1,12 @@
 # ledger/urls.py
 from django.urls import path
-from .views import index, RecipeListView, RecipeDetailView
+from .views import RecipeListView, RecipeDetailView, RecipeCreateView, add_image, add_recipe
 
 urlpatterns = [
-        path('', index, name='index'),
         path('recipes/list', RecipeListView.as_view(), name='recipes-list'),
         path('recipe/<int:pk>', RecipeDetailView.as_view(), name='recipe-detail'),
+        path('recipe/<int:pk>/add_image', add_image, name='add-image'),
+        path('recipe/add', add_recipe, name='recipe-create')
 ]
 
 app_name = "ledger"
--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -1,11 +1,13 @@
 # ledger/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView
 
-from .models import Recipe
+from .models import Recipe, RecipeImage
+from .forms import RecipeForm, RecipeImageForm
 
 class RecipeListView(ListView):
     model = Recipe
@@ -15,23 +17,53 @@ class RecipeDetailView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'recipe_details.html'
 
-def index(request):
-    return HttpResponse('Hello World! This ma recipe')
 
-def recipes_list(request):
-    recipes = Recipe.objects.all()
-    ctx = {
-    "recipes": recipes
-    }
+class RecipeImageCreateView(LoginRequiredMixin, CreateView):
+    model = RecipeImage
+    # template_name = '<model>task_form.html'
+    template_name = 'recipe_details.html'
+    form_class = RecipeImageForm
 
-    return render(request, 'recipes_list.html', ctx)
+    def get_success_url(self):
+        return f"/recipe/{self.kwargs['pk']}"  # redirect back to detail page
 
-def recipe(request):
-    recipe = Recipe.objects.all()
-    ctx = {
-        "recipe": recipe
-    }
-    return render(request, 'recipe_details.html', ctx)
+class RecipeCreateView(LoginRequiredMixin, CreateView):
+    model = Recipe
+    # template_name = '<model>task_form.html'
+    template_name = 'recipe_details.html'
+    form_class = RecipeForm
+
+
+def add_recipe(request):
+    form = RecipeForm()
+
+    if request.method == "POST":
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('ledger:recipes-list'))
+        else:
+            form = RecipeForm()
+            
+    ctx = {'form': form}
+
+    return render(request, 'add_forms.html', ctx)
+
+def add_image(request, pk):
+    form = RecipeImageForm()
+
+    if request.method == "POST":
+        form = RecipeImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('ledger:recipe-detail', args=[pk]))
+        else:
+            form = RecipeImageForm()
+            
+    ctx = {'form': form}
+
+    return render(request, 'add_forms.html', ctx)
+
 
 # Create your views here.
 
